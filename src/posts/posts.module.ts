8301bab5ffe5f1dc38f320b0fd54e703172beee2
Minh <223230201+BlackBeard1720@users.noreply.github.com>
2026-06-22 10:18:28 +0700

    add mockPostsService

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 
+const mockPostsService = {
+  findAll: () => [{ id: 1, title: 'Mock', content: 'Demo' }],
+}
 @Module({
   // Đăng ký controller và service cho PostsModule.
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [
+    {
+      provide: PostsService,
+      useValue: mockPostsService,
+    },
+  ],
 })
 export class PostsModule {}
